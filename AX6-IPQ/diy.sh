e9582ede335ac43bef47824f4083dfd526305394
g1422741595 <g1422741595@gmail.com>
2025-01-31 09:53:19 +0800

    修改

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -37,12 +37,12 @@ git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/l
 
 # 添加额外插件
 
-echo >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+#echo >> feeds.conf.default
+#echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #echo 'src-git autotimeset  https://github.com/sirpdboy/luci-app-autotimeset' >> feeds.conf.default
 
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
+#./scripts/feeds update istore
+#./scripts/feeds install -d y -p istore luci-app-store
 # 更新feeds，并安装主题：
 #./scripts/feeds update autotimeset
 #./scripts/feeds install luci-app-autotimeset
