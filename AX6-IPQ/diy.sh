79e8674f3220477280532859c467261d97607cfc
xuxihan <61369344+xuy132@users.noreply.github.com>
2025-03-05 15:18:41 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -106,7 +106,7 @@ echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >>
 
 
 #修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
 #修改主机名
 sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
