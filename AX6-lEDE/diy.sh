8b30a3e29e86abd5982b8992527936d4f85787b7
xuxihan <61369344+xuy132@users.noreply.github.com>
2025-03-15 04:25:48 +0800

    Update diy.sh

--- AX6-lEDE/diy.sh
+++ AX6-lEDE/diy.sh
@@ -41,10 +41,10 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 #rm -rf feeds/luci/applications/luci-app-design-config
 
 # Default IP
-#sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
 # 修改主机名
-#sed -i "s/hostname='OpenWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
 
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
